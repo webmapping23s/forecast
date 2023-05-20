c507c158afd35ee87be9cc9036b5521251662f8c
Klaus Förster <openwebcc@github.com>
2023-05-20 17:33:32 +0200

    WL8: Wettervorhersage Template auspacken

---
+++ main.js
@@ -0,0 +1,38 @@
+/* Wetterstationen Euregio Beispiel */
+
+// Innsbruck
+let ibk = {
+    lat: 47.267222,
+    lng: 11.392778
+};
+
+// Karte initialisieren
+let map = L.map("map", {
+    fullscreenControl: true
+}).setView([ibk.lat, ibk.lng], 5);
+
+// thematische Layer
+let themaLayer = {
+    forecast: L.featureGroup()
+}
+
+// Hintergrundlayer
+let layerControl = L.control.layers({
+    "Openstreetmap": L.tileLayer.provider("OpenStreetMap.Mapnik"),
+    "Esri WorldTopoMap": L.tileLayer.provider("Esri.WorldTopoMap"),
+    "Esri WorldImagery": L.tileLayer.provider("Esri.WorldImagery").addTo(map)
+}, {
+    "Wettervorhersage MET Norway": themaLayer.forecast.addTo(map)
+}).addTo(map);
+
+// Maßstab
+L.control.scale({
+    imperial: false,
+}).addTo(map);
+
+// Wettervorhersage MET Norway
+async function showForecast(url, latlng) {
+    let response = await fetch(url);
+    let jsondata = await response.json();
+    console.log(jsondata, latlng);
+}
