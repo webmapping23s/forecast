d6ab09c55cfe8422e1e761a2b6a2b322676bd6a2
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-05-25 15:56:12 +0200

    showForecast Funktion aufrufen und Ergebnis anzeigen

--- main.js
+++ main.js
@@ -40,5 +40,6 @@ async function showForecast(url, latlng) {
 // auf Kartenklick reagieren
 map.on("click", function(evt) {
     //console.log(evt.latlng.lat);
-    console.log(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`);
+    let url = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`;
+    showForecast(url, evt.latlng);
 });
