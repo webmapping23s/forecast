3bdc9cf6b6fbe05fec84ded172471efeae59c764
Klaus Förster <klaus.foerster@uibk.ac.at>
2023-05-25 15:51:37 +0200

    Eventhandler onclick für URL api.met.no

--- main.js
+++ main.js
@@ -36,3 +36,9 @@ async function showForecast(url, latlng) {
     let jsondata = await response.json();
     console.log(jsondata, latlng);
 }
+
+// auf Kartenklick reagieren
+map.on("click", function(evt) {
+    //console.log(evt.latlng.lat);
+    console.log(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${evt.latlng.lat}&lon=${evt.latlng.lng}`);
+});
